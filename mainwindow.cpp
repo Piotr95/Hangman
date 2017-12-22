e6d47508984ceb9eaa7f588166b8011c00390c42
Piotr Szczepaniak <piotrszczepniak@gmail.com>
2017-12-22 13:04:40 +0100

    word box stylization

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -18,16 +18,18 @@ MainWindow::MainWindow(QWidget *parent) :
 void MainWindow::decorate(QTextEdit *TE)
 {
 
-    TE->setReadOnly(true);
-    TE->setVerticalScrollBarPolicy( Qt::ScrollBarAlwaysOff);
-    TE->setHorizontalScrollBarPolicy( Qt::ScrollBarAlwaysOff);
-    TE->setMaximumSize(31,42);
-    TE->setMaximumSize(31,42);
-    TE->setAlignment(Qt::AlignCenter| Qt::AlignBottom);
-    TE->setPlaceholderText(" _ ");
-    TE->setFontPointSize(8);
-    TE->setFontFamily("MS Shell Dlg 2");
+ TE->setReadOnly(true);
+ TE->setMaximumSize(31,42);
+ TE->setPlaceholderText("D");
+ TE->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+ TE->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+ TE->setHtml(QApplication::translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:'MS Shell Dlg 2'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n"
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:16pt; font-weight:600;\">_</span></p></body></html>", Q_NULLPTR));
 }
+
 QTextEdit** MainWindow::initfields( int size_off_word)
 {
 
